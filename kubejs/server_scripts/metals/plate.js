fa3742e1f24cd70cf9fad57179b490602f8f5e1b
Winter0125 <89208092+Winter0125@users.noreply.github.com>
2022-11-19 19:52:32 +0900

    add plate.js update replace.js

--- kubejs/server_scripts/metals/plate.js
+++ kubejs/server_scripts/metals/plate.js
@@ -1,2 +1,22 @@
 let metals = ['bismuth', 'bismuth_bronze', 'black_bronze', 'black_steel', 'blue_steel', 'brass', 'bronze', 'cast_iron', 'copper', 'gold', 'nickel', 'red_steel', 'rose_gold', 'silver', 'steel', 
-'sterling_silver', 'tin', 'wrought_iron', 'zinc']
+        'sterling_silver', 'tin', 'wrought_iron', 'zinc']
+let ingots = []
+let sheets = []
+let doubleIngots = []
+metals.forEach(metal => {
+    ingots.push('tfc:metal/ingot/'+metal)
+})
+metals.forEach(metal => {
+    sheets.push('tfc:metal/sheet/'+metal)
+})
+metals.forEach(metal => {
+    doubleIngots.push('tfc:metal/double_ingot/'+metal)
+})
+
+
+onEvent('recipes', e => {
+    e.remove({output: '#forge:plates'})
+
+    e.recipes.immersiveengineeringMetalPress(sheet, '2x '+ingot, 'immersiveengineering:mold_plate').energy(1000)
+    e.recipes.createPressing(sheet, doubleIngots)
+})
