12b8919072c560e02d7f9961213ec0b155b633e9
Winter0125 <89208092+Winter0125@users.noreply.github.com>
2022-11-30 23:28:34 +0900

    add IE.js

--- kubejs/server_scripts/mods/sophisticated backpack/sophisticated backpack.js
+++ kubejs/server_scripts/mods/sophisticated backpack/sophisticated backpack.js
@@ -1,12 +1,3 @@
 onEvent('recipe', e => {
-    e.remove({id: 'sophisticatedbackpacks:backpack'})
-    e.remove({id: 'sophisticatedbackpacks:upgrade_base'})
-    e.remove({output:'sophisticatedbackpacks:backpack'})
-    e.remove({output:'sophisticatedbackpacks:upgrade_base'})
-    
-    e.shaped('sophisticatedbackpacks:backpack', ['SLS', 'SCS', 'LLL'], {
-        S: 'tfc:wool_yarn',
-        L: 'minecraft:leather',
-        C: '#minecraft:chest'
-    })
+
 })
