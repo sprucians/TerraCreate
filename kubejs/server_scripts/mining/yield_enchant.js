520fb923986d01ef84aea161d5aad120848d6fa7
ZoniIC645 <michael20030908@gmail.com>
2023-08-15 00:43:07 +0900

    ore enchant bug fix

--- kubejs/server_scripts/mining/yield_enchant.js
+++ kubejs/server_scripts/mining/yield_enchant.js
@@ -3,7 +3,7 @@
 onEvent('recipes', event => {
 	// Change recipes here
 	//console.info('=====================================================================================Add ore smelting recipe!!!!!!')
-	event.forEachRecipe({type : 'tfc:heating'}, recipe => {
+	event.forEachRecipe({ type: 'tfc:heating' }, recipe => {
 		if (!recipe.json.asJsonObject.has('ingredient') || !recipe.json.asJsonObject.has('result_fluid'))
 			return;
 		let item = recipe.json.asJsonObject.get('ingredient').get('item')
@@ -11,25 +11,26 @@ onEvent('recipes', event => {
 		let amount = recipe.json.asJsonObject.get('result_fluid').get('amount') * 1
 		let temp = recipe.json.asJsonObject.get('temperature')
 		if (Item.of(item) && Item.of(item).hasTag('tfc:ore_pieces')) {
-			//event.remove(recipe)
-			for (let i in [0,1,2]){
+			event.remove({ input: item, type: "tfc:heating" })
+			for (let i of [0, 1, 2]) {
 				event.custom({
 					"type": "create:mixing",
 					"ingredients": [
-						i == 0 ? Item.of(item).toJson() : Item.of(item).enchant('kubejs:yield',i).toJson()
+						i == 0 ? Item.of(item).toJson() : Item.of(item).enchant('kubejs:yield', i).toJson()
 					],
 					"results": [
-						Fluid.of(fluid, amount*(1 + 0.5*i)).toJson()
+						Fluid.of(fluid, amount * (1 + 0.5 * i)).toJson()
 					],
 					"heatRequirement": "heated"
 				})
-
+			}
+			for (let i of [1, 2]) {
 				event.custom({
 					"type": "tfc:heating",
-					"ingredient": i == 0 ? Item.of(item).toJson() : Item.of(item).enchant('kubejs:yield',i).toJson(),
+					"ingredient": i == 0 ? Item.of(item).toJson() : Item.of(item).enchant('kubejs:yield', i).toJson(),
 					"result_fluid": {
 						"fluid": fluid,
-						"amount": amount*(1 + 0.5*i)
+						"amount": amount * (1 + 0.5 * i)
 					},
 					"temperature": temp
 				})
